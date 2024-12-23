64f429147cc19df059926cb17d83c2ed7b9f9316
Ori Kanner <orikanner@Mac.lan>
2024-12-23 15:15:30 +0200

    receive also files' last modified date, validate and convert from timestamp

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -4,6 +4,7 @@ import {
   UploadedFiles,
   UseInterceptors,
   BadRequestException,
+  Body,
 } from '@nestjs/common';
 import { FilesInterceptor } from '@nestjs/platform-express';
 import { memoryStorage } from 'multer';
@@ -11,6 +12,11 @@ import {
   FILES_ALLOWED_EXTENSIONS,
   FILE_SIZE_LIMIT,
 } from '../utils/constants.js';
+
+interface datesModifiedInterface {
+  lastModified: number[];
+}
+
 @Controller('files')
 export class FilesController {
   constructor() {}
@@ -71,6 +77,27 @@ export class FilesController {
     };
   }
 
+  validateTimestampAndConvertToDate(timestamp: string | number): {
+    valid: boolean;
+    error?: string;
+    data?: Date;
+  } {
+    // Validate TimeStamp
+    if (isNaN(Number(timestamp))) {
+      return { valid: false, error: 'Invalid lastModified timestamp' };
+    }
+    const lastModifiedDate = new Date(Number(timestamp));
+    // Validate date
+    if (isNaN(lastModifiedDate.getTime())) {
+      return { valid: false, error: 'Invalid date derived from timestamp' };
+    }
+    // Validate date is in the past
+    if (lastModifiedDate > new Date()) {
+      return { valid: false, error: 'Timestamp cannot be in the future' };
+    }
+    return { valid: true, data: lastModifiedDate };
+  }
+
   @Post('upload')
   @UseInterceptors(
     FilesInterceptor('files', null, {
@@ -84,7 +111,11 @@ export class FilesController {
       preservePath: true,
     }),
   )
-  uploadFiles(@UploadedFiles() files: Array<Express.Multer.File>) {
+  uploadFiles(
+    @UploadedFiles() files: Array<Express.Multer.File>,
+    @Body() { lastModified }: datesModifiedInterface,
+  ) {
+    console.log('dates', lastModified);
     try {
       const errors = [];
       const filesSummary = [];
@@ -93,15 +124,36 @@ export class FilesController {
         throw new BadRequestException('No files uploaded');
       }
       // Got files
-      for (let file of files) {
-        const isFileValid = this.validateFile(file);
+      for (let fileIndex = 0; fileIndex < files.length; fileIndex++) {
+        console.log(
+          files[fileIndex],
+          '-',
+          new Date(Number(lastModified[fileIndex])),
+        );
+        // Validate file's last modified timestamp
+        const lastModifiedDate = this.validateTimestampAndConvertToDate(lastModified[fileIndex]);
+        if (!lastModifiedDate.valid) {
+          errors.push(lastModifiedDate.error)
+          continue;
+        }
+        const isFileValid = this.validateFile(files[fileIndex]);
         if (!isFileValid.valid) {
           errors.push(isFileValid);
           continue;
         }
+        // TODO: Check if the customer is already in the file, add it or update it with the relevant information from the file
+        
+        // Parse and validate file content - required columns for xls or proper document structure
 
-        // TODO: Check if the customer is already in the file, add or update it with the relevant information from the file
-        // TODO2: Parse the documents and start filling the matrix with the customers names and statuses
+        // Connect to my local db (SQL)
+
+        // If the file's last modified date is older than the last update time in the database, skip processing.        
+
+        // Else start a transaction.
+        
+        // Update the necessary rows based on the file data.
+        
+        // Commit the transaction only if all rows are successfully updated.        
       }
 
       return {
